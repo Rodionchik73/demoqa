dbc646e5553d8c720b6c4c7a44d09de344d12a48
Rodionchik73 <olelik260591@gmail.com>
2025-01-21 23:19:01 +0300

    Initial commit

---
+++ src/test/java/org/example/demoqa/MainPageTest.java
@@ -0,0 +1,44 @@
+package org.example.demoqa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.get("https://demoqa.com/dynamic-properties");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void visible() {
+        WebDriverWait wait = new WebDriverWait (driver,Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#visibleAfter")));
+        WebElement visibleButton = driver.findElement(By.cssSelector("#visibleAfter"));
+        assertTrue(visibleButton.isDisplayed(), "Кнопка не отображается на странице");
+    }
+}
